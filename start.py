f1020b977d70bd25dae061a4c425ad343d1d1ad1
harikapedapudi99 <146189075+harikapedapudi99@users.noreply.github.com>
2023-10-05 11:15:40 +0530

    Update start.py

--- start.py
+++ start.py
@@ -142,16 +142,11 @@ def init_session():
 
 if __name__ == '__main__':
     hide_streamlit_style = """
-
-                <style>
-
-                #MainMenu {visibility: hidden;}
-
-                footer {visibility: hidden;}
-
-                </style>
-
-                """
+        <style>
+        #MainMenu {visibility: hidden;}
+        footer {visibility: hidden;}
+        </style>
+        """
 
     st.markdown(hide_streamlit_style, unsafe_allow_html=True)
     if 'SNOWPARK_SESSION' not in st.session_state:
